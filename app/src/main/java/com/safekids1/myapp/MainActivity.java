f9b1f6d8377ad07bb1dc4ea90b0885a2380bb2d6
praisegod2448-droid <praisegod2448@gmail.com>
2026-07-09 13:54:45 -0400

    Implement onCreate method in MainActivity
    Override onCreate method to initialize activity.

--- app/src/main/java/com/safekids1/myapp/MainActivity.java
+++ app/src/main/java/com/safekids1/myapp/MainActivity.java
@@ -1,5 +1,13 @@
 package com.safekids1.myapp;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Native driver plugins link and load automatically at application runtime.
+    }
+}
