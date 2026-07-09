efd6e851e12dff0b60bf95eacd474438c2c92d2c
praisegod2448-droid <praisegod2448@gmail.com>
2026-07-09 13:49:17 -0400

    Implement Geolocation plugin registration in MainActivity
    Added onCreate method to register GeolocationPlugin.

--- android/app/src/main/java/com/safekids1/myapp/MainActivity.java
+++ android/app/src/main/java/com/safekids1/myapp/MainActivity.java
@@ -1,5 +1,17 @@
 package com.safekids1.myapp;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // FORCED INJECTION: Registers the hardware Geolocation tracking driver manually 
+        // into Capacitor's startup engine layer so web layout buttons can talk to the phone GPS!
+        registerPlugin(com.getcapacitor.plugins.geolocation.GeolocationPlugin.class);
+    }
+}
